8dd3cf1a29cb35c116b11a0200ef166095613293
ripercheto <pavel.stoimenov@abv.bg>
2024-08-18 16:50:36 +0200

    asdasd

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using Sirenix.OdinInspector;
 using SmallHedge.SoundManager;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Rendering;
 
